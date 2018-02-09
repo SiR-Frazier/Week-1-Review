c5bc6efdd9f0fba7bce0ae072a594b1df0ff733c
Bryce Frazier <bx.frazier@gmail.com>
2018-02-09 14:41:42 -0800

    passed first test

--- lib/code_review1.rb
+++ lib/code_review1.rb
@@ -1,6 +1,26 @@
-# Class Words
-#   def initialize(word1, word2)
-#     @word1 = word1
-#     @word2 = word2
-#   end
-# end
+class Words
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+
+  # def word1
+  #   @word1
+  # end
+  #
+  # def word2
+  #   @word2
+  # end
+
+  def anagram_check(word1, word2)
+    word1_array = word1.split("")
+    word2_array = word2.split("")
+    word1_sort = word1_array.sort
+    word2_sort = word2_array.sort
+    if (word1_sort == word2_sort)
+      "These words are anagrams"
+    else
+     "These words are not anagrams"
+    end
+  end
+end
--- spec/code_review1_spec.rb
+++ spec/code_review1_spec.rb
@@ -2,9 +2,9 @@ require('rspec')
 require('code_review1')
 require('pry')
 
-describe("#code_review1") do
+describe("#anagram") do
   it("returns message 'These words are anagrams' if the two input words contain the same letters in a different order") do
-    words = Word.new(word1, word2)
-    expect(words.is_anagram("dog","god")).to(eq("These words are anagrams"))
+    words = Words.new(@word1, @word2)
+    expect(words.anagram_check("dog","god")).to(eq("These words are anagrams"))
   end
 end
