5226e2ad60d4742cf54bed1469e4621a0e489f9d
sir-frazier <bxfrazier@gmail.com>
2018-02-12 21:52:16 -0800

    simply code for downcase and remove downcase method

--- lib/code_review1.rb
+++ lib/code_review1.rb
@@ -13,8 +13,8 @@ class Words
   # end
 
   def anagram_check(word1, word2)
-    word1_array = word1.split("")
-    word2_array = word2.split("")
+    word1_array = word1.downcase.split("")
+    word2_array = word2.downcase.split("")
     word1_sort = word1_array.sort
     word2_sort = word2_array.sort
     if (word1_sort == word2_sort)
@@ -24,21 +24,21 @@ class Words
     end
   end
 
-  def lowercase_check(word1, word2)
-    new_word1 = word1.downcase.split("")
-    new_word2 = word2.downcase.split("")
-    new_word1_sort = new_word1.sort
-    new_word2_sort = new_word2.sort
-    if (new_word1_sort == new_word2_sort)
-      "These words are anagrams"
-    else
-      "These words are not anagrams"
-    end
-  end
+  # def lowercase_check(word1, word2)
+  #   new_word1 = word1.downcase.split("")
+  #   new_word2 = word2.downcase.split("")
+  #   new_word1_sort = new_word1.sort
+  #   new_word2_sort = new_word2.sort
+  #   if (new_word1_sort == new_word2_sort)
+  #     "These words are anagrams"
+  #   else
+  #     "These words are not anagrams"
+  #   end
+  # end
 
   def vowel_check(word1, word2)
     if !word1.scan(/[aeiou]/) || word2.scan(/[aeiou]/)
-      "Please enter actual words"
+      "You need to submit actual words"
     end
   end
 end
--- spec/code_review1_spec.rb
+++ spec/code_review1_spec.rb
@@ -7,12 +7,15 @@ describe("#anagram") do
     words = Words.new(@word1, @word2)
     expect(words.anagram_check("dog","god")).to(eq("These words are anagrams"))
   end
-  it("returns message 'These words are anagrams' if the two input words contain the same letters regardless of order or case ") do
-    words = Words.new(@word1, @word2)
-    expect(words.lowercase_check("Cat","AcT")).to(eq("These words are anagrams"))
-  end
+
+  # it("returns message 'These words are anagrams' if the two input words contain the same letters regardless of order or case ") do
+  #   words = Words.new(@word1, @word2)
+  #   expect(words.lowercase_check("Cat","AcT")).to(eq("These words are anagrams"))
+  # end
+
   it("returns 'You need to submit actual words' if there are no vowels included in the words") do
     words = Words.new(@word1, @word2)
-    expect(words.vowel_check("yrrb","rbyr")).to(eq("Please enter actual words"))
+    expect(words.vowel_check("yrrb","rbyr")).to(eq("You need to submit actual words"))
   end
+
 end
